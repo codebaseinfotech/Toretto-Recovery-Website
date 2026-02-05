cd38d18d8055c1753d37a4d723ae1460957df5d6
shwetacodebase-hue <shweta.codebase@gmail.com>
2026-02-05 11:03:15 +0530

    COMMIT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,4 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->withBindings([
+        'path.storage' => function () {
+            return dirname(__DIR__) . '/storage';
+        },
+    ])->create();
